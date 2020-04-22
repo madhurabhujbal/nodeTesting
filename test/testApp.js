3d60501d2f324423bd10ab663707d103942a40a6
Madhura <madhura.bhujbal@gmail.com>
2020-04-22 17:00:14 +0100

    Rearrange tests under respective functions

--- test/testApp.js
+++ test/testApp.js
@@ -5,27 +5,32 @@ const assert = require('chai').assert;
 
 sayHelloResult = app.sayHello();
 addNumbersResult = app.addNumbers(4,8);
+
 //creating a test
 describe('App', function() {
     //testing the actual value
-    it('sayHello should return hello', function() {
-        let result = app.sayHello();
-        assert.equal(sayHelloResult, 'hello');
+    describe('sayHello()', function() {
+        it('sayHello should return hello', function() {
+            let result = app.sayHello();
+            assert.equal(sayHelloResult, 'hello');
+        });
+
+        it('sayHello should return a string', function() {
+            let result = app.sayHello();
+            assert.typeOf(sayHelloResult, 'string');
+        });
     });
 
-    it('sayHello should return a string', function() {
-        let result = app.sayHello();
-        assert.typeOf(sayHelloResult, 'string');
-    });
+    describe('addNumbers()', function() {
+        it('addNumbers should return sum of 2 numbers greater than 5', function() {
+            let result = app.addNumbers(4,8);
+            assert.isAbove(addNumbersResult, 5);
+        });
 
-    it('addNumbers should return sum of 2 numbers greater than 5', function() {
-        let result = app.addNumbers(4,8);
-        assert.isAbove(addNumbersResult, 5);
+        it('addNumbers should return a number', function() {
+            let result = app.addNumbers(4,8);
+            assert.typeOf(addNumbersResult, 'number');
+        });
 
-    });
-
-    it('addNumbers should return a number', function() {
-        let result = app.addNumbers(4,8);
-        assert.typeOf(addNumbersResult, 'number');
     });
 });
