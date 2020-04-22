36a2b86370a4878f87a2a030890fb45b96d9fed7
Madhura <madhura.bhujbal@gmail.com>
2020-04-22 16:53:21 +0100

    Store result of sayHello and addNumbers function into variables and use those variables for evaluating conditions

--- test/testApp.js
+++ test/testApp.js
@@ -3,27 +3,29 @@ const app = require('../app');
 // const addNumbers = require('../app').addNumbers;
 const assert = require('chai').assert;
 
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(4,8);
 //creating a test
 describe('App', function() {
     //testing the actual value
     it('sayHello should return hello', function() {
         let result = app.sayHello();
-        assert.equal(result, 'hello');
+        assert.equal(sayHelloResult, 'hello');
     });
 
     it('sayHello should return a string', function() {
         let result = app.sayHello();
-        assert.typeOf(result, 'string');
+        assert.typeOf(sayHelloResult, 'string');
     });
 
     it('addNumbers should return sum of 2 numbers greater than 5', function() {
         let result = app.addNumbers(4,8);
-        assert.isAbove(result, 5);
+        assert.isAbove(addNumbersResult, 5);
 
     });
 
     it('addNumbers should return a number', function() {
         let result = app.addNumbers(4,8);
-        assert.typeOf(result, 'number');
+        assert.typeOf(addNumbersResult, 'number');
     });
 });
